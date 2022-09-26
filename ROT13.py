2b77d7ef4ed4a69ec4eab0a4e84b9d11840f42f7
Pablo Angel <angel.pablo20@gmail.com>
2022-09-26 13:13:23 -0300

    Se agrega proyecto Lunes 26

---
+++ ROT13.py
@@ -0,0 +1,7 @@
+UNCYPHERED ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+ROT13="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+def rot13(message):
+    ciphered_str = message.translate(message.maketrans(UNCYPHERED, ROT13))
+    print(ciphered_str)
+
+rot13(input("Ingresa un texto : "))
